70ab85c9ff824b57024187cb09a2ffaf23f0ab4c
Casper <32563753+MegaRandomCake@users.noreply.github.com>
2017-11-30 22:53:04 +0100

    Javadoc for Cup

--- src/entity/Cup.java
+++ src/entity/Cup.java
@@ -1,22 +1,41 @@
-/**
- * 
- */
 package entity;
 
-/**
- * @author Stonecore
- *
- */
+	/**
+	 * Provides a six-sided die  with metods for rolling the die,
+	 * and returning the faceup value of the die.
+	 * This class should only be imported from the controller.Controller class
+	 */
 public class Cup {
 	private int eyes;
+	
+	/**
+	 * Sets the value of the die to a random number from [1,2,3,4,5,6]
+	 * 
+	 * @see Cup#Dice()
+	 * @param eyes The current faceup-value of the die
+	 */
 
 	public void rollCup() {
 		eyes = Dice();
 	}
+	
+	/**
+	 * Returns the current faceup-value of the die
+	 * 
+	 * @param eyes The current faceup-value
+	 * @return The current faceup-value of the die
+	 */
 
 	public int getEyes() {
 		return eyes;
 	}
+	
+	/**
+	 * Returns a pseudorandom number from [1,2,3,4,5,6] with Math.random
+	 * The method is only used in the Cup.rollCup() method
+	 * 
+	 * @return A number from 1 to 6.
+	 */
 
 	private int Dice() {
 		return (int) ((Math.random() * 6) + 1);
